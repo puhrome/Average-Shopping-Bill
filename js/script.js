03138fde518ea2da3426266f52424d1ec6f9f681
puhrome <phirom.yim@fullsail.edu>
2014-04-13 10:36:22 -0400

    Average Shopping Bill
    Using WebStorm to create arrays and average expressions in HTML and JS.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ *Average Shopping Bill Created by Phirom Yim on 4/12/14.
+ */
+var grocery = [5.55, 2.75, 8.96, 19.97];
+grocery[0] = 5.55;
+grocery[1] = 2.75;
+grocery[2] = 8.96;
+grocery[3] = 19.97;
+var total= grocery[0]+grocery[1]+grocery[2]+grocery[3];
+console.log(total);
+var average = total/4;
+console.log(average);
