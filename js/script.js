fd89e7ea1d5be9a0a2bf21c3b98cc41fd3d9e747
puhrome <phirom.yim@fullsail.edu>
2014-04-13 10:41:12 -0400

    Added Comments
    Declared and Defined expression

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 /**
  *Average Shopping Bill Created by Phirom Yim on 4/12/14.
  */
-var grocery = [5.55, 2.75, 8.96, 19.97];
-grocery[0] = 5.55;
-grocery[1] = 2.75;
-grocery[2] = 8.96;
-grocery[3] = 19.97;
-var total= grocery[0]+grocery[1]+grocery[2]+grocery[3];
-console.log(total);
-var average = total/4;
-console.log(average);
+var grocery = [5.55, 2.75, 8.96, 19.97]; //declare list of grocery cost
+grocery[0] = 5.55; //first item in list always starts with 0
+grocery[1] = 2.75;// second item in list
+grocery[2] = 8.96;// third item in list
+grocery[3] = 19.97;//fourth item in list
+//total of grocery bill is [0-3]
+var total= grocery[0]+grocery[1]+grocery[2]+grocery[3]; //declaring array list
+console.log(total);//print
+//total of grocery bill divided by the number of items in the list is equal to the average
+var average = total/4;//declaring expression
+console.log(average);//print
